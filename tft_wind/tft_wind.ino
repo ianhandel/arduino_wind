ec156a42f4b4e4c1cf72616f961d5936ec7c9ba5
Ian Handel <ian.handel@ed.ac.uk>
2018-02-28 15:57:23 +0000

    start from example

---
+++ tft_wind/tft_wind.ino
@@ -0,0 +1,62 @@
+#include <Adafruit_GFX.h>    // Core graphics library
+#include <Adafruit_TFTLCD.h> // Hardware-specific library
+
+// The control pins for the LCD can be assigned to any digital or
+// analog pins...but we'll use the analog pins as this allows us to
+// double up the pins with the touch screen (see the TFT paint example).
+#define LCD_CS A3 // Chip Select goes to Analog 3
+#define LCD_CD A2 // Command/Data goes to Analog 2
+#define LCD_WR A1 // LCD Write goes to Analog 1
+#define LCD_RD A0 // LCD Read goes to Analog 0
+
+#define LCD_RESET A4 // Can alternately just connect to Arduino's reset pin
+
+// Assign human-readable names to some common 16-bit color values:
+#define	BLACK   0x0000
+#define	BLUE    0x001F
+#define	RED     0xF800
+#define	GREEN   0x07E0
+#define CYAN    0x07FF
+#define MAGENTA 0xF81F
+#define YELLOW  0xFFE0
+#define WHITE   0xFFFF
+
+Adafruit_TFTLCD tft(LCD_CS, LCD_CD, LCD_WR, LCD_RD, LCD_RESET);
+// If using the shield, all control and data lines are fixed, and
+// a simpler declaration can optionally be used:
+// Adafruit_TFTLCD tft;
+
+void setup(void) {
+  tft.reset();
+
+  //uint16_t identifier = tft.readID();
+  uint16_t identifier = 0x9341;
+
+  tft.begin(identifier);
+  tft.fillScreen(BLACK);
+}
+
+void loop(void) {
+  text();
+}
+
+void text() {
+  for (uint8_t i=0; i<4; i++) {
+    tft.fillScreen(BLACK);
+
+    tft.setCursor(0, 30);
+    tft.setTextColor(RED);
+    tft.setTextSize(1);
+    tft.println("Hello World!");
+    tft.setTextColor(YELLOW);
+    tft.setTextSize(2);
+    tft.println("Hello World!");
+    tft.setTextColor(GREEN);
+    tft.setTextSize(3);
+    tft.println("Hello World!");
+    tft.setTextColor(BLUE);
+    tft.setTextSize(4);
+    tft.print(1234.567);
+  }
+}
+
